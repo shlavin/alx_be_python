a0fad494ad63f907b1f35b0c1e6406fe70a952da
Shlavin <nyamburawahu16@gmail.com>
2025-09-27 15:14:38 +0300

    modified arithmetic.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,15 @@
-def perform_operation( num1, num2, operation):
+def perform_operation(num1, num2, operation):
     match operation:
         case "add":
             return num1 + num2
         case "subtract":
-           return num1 - num2
+            return num1 - num2
         case "multiply":
             return num1 * num2
         case "divide":
             if num2 == 0:
-                return("Error: You cannot divide by zero")
+                return "Error: You cannot divide by zero"
             else:
-                return num1 / num2      
-        case _ : 
-           return ("Error: Invalid operation selected")       
+                return num1 / num2
+        case _:
+            return "Error: Invalid operation selected"
