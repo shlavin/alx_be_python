345e5d6c41a13b142c1f90115cccd0300b815298
Shlavin <nyamburawahu16@gmail.com>
2025-09-27 14:38:12 +0300

    added the folder fns and dsa and inside it a file arithmeti_operations,py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation( num1:float , num2:float , operation:str):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+           return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                return("You cannot divide by zero")
+            else:
+                return num1 / num2      
+        case _ : 
+           return ("Invalid operation selected")       
